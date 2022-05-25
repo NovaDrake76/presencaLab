0d0df9a37edeab6994ebb8fdd082d68ea5f6c7ae
Nathan <novadrake76@gmail.com>
2022-05-25 20:21:56 -0300

    radio butons fixed

--- presenca.php
+++ presenca.php
@@ -1,78 +1,80 @@
 <?php
 require_once "config.php";
 date_default_timezone_set('America/Sao_Paulo');
- 
+
 $nome = $matricula = $dia = $turno = $curso = "";
 $nome_err = $matricula_err =  $curso_err = "";
 
 $turnoAux = date("H");
 
-if($turnoAux < 12){
+if ($turnoAux < 12) {
     $turno = "Manhã";
-}else if($turnoAux >= 13 && $turnoAux < 18){
+} else if ($turnoAux >= 13 && $turnoAux < 18) {
     $turno = "Tarde";
-}else{
+} else {
     $turno = "Noite";
 }
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $input_nome = trim($_POST["nome"]);
-    if(empty($input_nome)){
+    if (empty($input_nome)) {
         $nome_err = "Digite um nome válido.";
-    } elseif(!filter_var($input_nome, FILTER_VALIDATE_REGEXP, array("options"=>array("regexp"=>"/^[a-zA-Z\s]+$/")))){
+    } elseif (!filter_var($input_nome, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^[a-zA-Z\s]+$/")))) {
         $nome_err = "Digite um nome válido.";
-    } else{
+    } else {
         $nome = $input_nome;
     }
-    
+
     $input_matricula = trim($_POST["matricula"]);
     $matricula = $input_matricula;
-    
+
     $input_curso = trim($_POST["curso"]);
     $curso = $input_curso;
 
-    if(empty($nome_err) && empty($matricula_err)){
+    if (empty($nome_err) && empty($matricula_err)) {
         $sql = "INSERT INTO usuarios (nome, matricula, turno, curso) VALUES (?, ?, ?, ?)";
-         
-        if($stmt = mysqli_prepare($link, $sql)){
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
             mysqli_stmt_bind_param($stmt, "siss", $param_nome, $param_matricula, $param_turno, $param_curso);
-            
-           
+
+
             $param_nome = $nome;
             $param_matricula = $matricula;
             $param_turno = $turno;
             $param_curso = $curso;
-          
-            if(mysqli_stmt_execute($stmt)){
-   
+
+            if (mysqli_stmt_execute($stmt)) {
+
                 header("location: presenca.php");
                 exit();
-            } else{
+            } else {
                 echo "Algo deu errado, tente novamente.";
             }
         }
-         
-       
+
+
         mysqli_stmt_close($stmt);
     }
-    
+
     mysqli_close($link);
 }
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Deixe sua presença</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
-        .wrapper{
+        .wrapper {
             width: 600px;
             margin: 0 auto;
         }
     </style>
 </head>
+
 <body>
     <div class="wrapper">
         <div class="container-fluid">
@@ -83,46 +85,41 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                         <div class="form-group">
                             <label>Nome</label>
                             <input type="text" name="nome" class="form-control <?php echo (!empty($nome_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $nome; ?>">
-                            <span class="invalid-feedback"><?php echo $nome_err;?></span>
+                            <span class="invalid-feedback"><?php echo $nome_err; ?></span>
                         </div>
                         <div class="form-group">
                             <label>Matrícula (opcional)</label>
                             <input type="number" name="matricula" class="form-control <?php echo (!empty($matricula_err)) ? 'is-invalid' : ''; ?>"><?php echo $matricula; ?></input>
-                            <span class="invalid-feedback"><?php echo $matricula_err;?></span>
+                            <span class="invalid-feedback"><?php echo $matricula_err; ?></span>
                         </div>
-                        
+
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="pedagogia" id="pedagogia">
+                            <input class="form-check-input" type="radio" name="pedagogia" id="flexRadioDefault1" checked>
                             <label class="form-check-label" for="flexRadioDefault1">
                                 Faço Pedagogia
                             </label>
                         </div>
                         <div class="form-check">
-                            <input class="form-check-input" type="radio" name="educacao" id="educacao">
-                            <label class="form-check-label" for="flexRadioDefault1">
+                            <input class="form-check-input" type="radio" name="graduacao" id="flexRadioDefault2">
+                            <label class="form-check-label" for="flexRadioDefault2">
                                 Faço Pós-Graduação em Educação
                             </label>
                         </div>
-                        <div class="form-check">
-                            <input class="form-check-input" type="radio" name="outro" id="outro">
-                            <label class="form-check-label" for="flexRadioDefault1">
-                                Faço outro curso:
-                            </label>
+                        <div class="form-group mt-2">
+
+                            <input type="text" name="curso" placeholder="Outro curso" class="form-control"> </input>
+                            <span class="invalid-feedback"><?php echo $curso_err; ?></span>
                         </div>
-                        <div class="form-group">
-                            
-                            <input type="text" name="curso" placeholder="Qual o curso?" class="form-control"></input>
-                            <span class="invalid-feedback"><?php echo $curso_err;?></span>
-                        </div>
-              
+
                         <div class="mt-3">
                             <input type="submit" class="btn btn-primary " value="Enviar">
-                        <a href="index.php" class="btn btn-secondary ml-2">Ver Lista</a>
+                            <a href="index.php" class="btn btn-secondary ml-2">Ver Lista</a>
                         </div>
                     </form>
                 </div>
-            </div>        
+            </div>
         </div>
     </div>
 </body>
+
 </html>
